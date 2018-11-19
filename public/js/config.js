fc61dd233b877f20dad547f0c922b4851830f3c1
Abraham <abrahamxjmv@gmail.com>
2018-11-19 09:55:58 -0700

    Cambios en la url de la api de producción

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 if(document.URL.indexOf("localhost")>=0)
 	var url = "http://localhost:3000";
 else
-	var url = "https://rest-inventario.herokuapp.com/";
+	var url = "https://rest-inventario.herokuapp.com";
 
 const URLAPI = url;
